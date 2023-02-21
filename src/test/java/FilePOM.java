9197affa964c31667d081d9ff5d914c302cc2411
HazemSerdah <61620915+001hazem@users.noreply.github.com>
2023-02-21 12:47:41 +0200

    update World Market

--- src/test/java/FilePOM.java
+++ src/test/java/FilePOM.java
@@ -4,19 +4,19 @@ import org.openqa.selenium.WebElement;
 public class FilePOM {
 
     static public WebElement Button_Is_Cancel(){
-        return HomePage.driver.findElement(By.xpath("//*[@id=\"cpwm-additionaldisclaimer3\"]/button"));
+        return SetUp.driver.findElement(By.xpath("//*[@id=\"cpwm-additionaldisclaimer3\"]/button"));
     }
     static  public WebElement Search(){
-        return HomePage.driver.findElement(By.name("keyword"));
+        return SetUp.driver.findElement(By.name("keyword"));
     }
     static  public WebElement ClickOnButtonSearch(){
-        return HomePage.driver.findElement(By.xpath("//*[@id=\"searchBtn\"]"));
+        return SetUp.driver.findElement(By.xpath("//*[@id=\"searchBtn\"]"));
     }
     static public  WebElement ClickOnButtonSigIn(){
-        return  HomePage.driver.findElement(By.id("ml-topnav-identity-link"));
+        return  SetUp.driver.findElement(By.id("ml-topnav-identity-link"));
     }
     static public  WebElement ClickOnButtonCreateAccount(){
-        return  HomePage.driver.findElement(By.xpath("//*[@id=\"loggedin_panel\"]/div/div/div[2]/button[2]"));
+        return  SetUp.driver.findElement(By.xpath("//*[@id=\"loggedin_panel\"]/div/div/div[2]/button[2]"));
     }
 
 
--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -1,35 +1,28 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class HomePage {
 
-    protected static ChromeDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.MINUTES);
-        driver.get("https://www.worldmarket.com/");
-    }
-
     @Test(priority = 1)
     public void VerifyTheTitleIsCorrect() {
         String expectedResult = "Furniture, Home Decor, Rugs, Unique Gifts | World Market";
-        String ActualResult = driver.getTitle();
+        String ActualResult = SetUp.driver.getTitle();
         System.out.println("The title is " + ActualResult);
         Assert.assertEquals(ActualResult, expectedResult);
     }
@@ -44,18 +37,26 @@ public class HomePage {
 
     @Test(priority = 5)
    public void checkTheSearchIsFunctionality() {
+        String currentExpectedUrl = "https://www.worldmarket.com/search.do?pageName=&keyword=Gifts";
+
         WebElement search = FilePOM.Search();
         search.sendKeys("Gifts");
 
         WebElement clickButton = FilePOM.ClickOnButtonSearch();
         Assert.assertTrue(clickButton.isDisplayed());
         clickButton.click();
+        SetUp.driver.manage().timeouts().implicitlyWait(2, TimeUnit.MINUTES);
+
+        String currentUrl = SetUp.driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        Assert.assertEquals(currentExpectedUrl,currentUrl);
+
     }
 
 
     @AfterClass
     void tearDown() {
-        driver.quit();
+        SetUp.driver.quit();
     }
 
 }
